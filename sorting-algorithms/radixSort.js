b0a97c9168b7d35e3655197d9774bb912fb7a5e0
Chandra Lama <iamme24cl@gmail.com>
2021-10-05 16:09:37 -0700

    done

---
+++ sorting-algorithms/radixSort.js
@@ -0,0 +1,22 @@
+/**
+A significant number is one that does not have a zero at the beginning.
+
+20: two significant numbers, 2 and 0. The least significant number is 0, the
+most significant number is 2.
+
+02: one significant number: 2. The least significant number is 2.
+
+12.005: five significant numbers: 2, 2, 0, 0, and 5. The least significant
+number is 5, the most significant number is 1.
+
+Radix sort is used to sort numbers, and works by sorting the least significant
+number to the most significant number.
+
+RadixSort - Time Complexity
+Time: best - O(n + k), Avg - O(n + k), Worse n- O(n^2)
+Space: Worst O(n)
+**/
+
+function radixSort(array) {
+  
+}
