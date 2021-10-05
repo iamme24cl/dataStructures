364b100e46d06bdb204323bd42968ba5b2fa1810
Chandra Lama <iamme24cl@gmail.com>
2021-10-04 17:29:46 -0700

    done

---
+++ mergeSort.js
@@ -0,0 +1,36 @@
+/**
+MergeSort - Time Complexity
+Time: best - O(n log(n)), Avg - O(n log(n)), Worse n- O(n log(n))
+Space: Worst O(n)
+**/
+function mergeSort(array) {
+  if (array.length === 1) {
+    return array;
+  }
+
+  const middle = Math.floor(array.length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
+  return merge(
+    mergeSort(left),
+    mergeSort(right)
+  );
+}
+
+function merge(left, right) {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
